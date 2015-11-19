90675ff29509b45c6cfbfee2e3a52f3c06fd3ca2
Fadion Dashi <jonidashi@gmail.com>
2015-11-19 19:54:43 +0100

    Update formatting
    Signed-off-by: Fadion Dashi <jonidashi@gmail.com>

--- src/Authenticate.php
+++ src/Authenticate.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Fadion\Raiffeisen;
 
 /**
@@ -8,155 +9,136 @@ namespace Fadion\Raiffeisen;
  * Gateway i Raiffeisen kerkon per te autorizuar
  * kerkesen. Kthen nje array me te dhenat per tu
  * perdorur ne formen e kerkeses.
- *
- * @package Raiffeisen\Authenticate
- * @author Fadion Dashi
- * @version 1.1.0
  */
 class Authenticate
 {
+	/**
+	 * @var string ID e tregtarit. Jepet nga banka
+	 */
+	private $merchantId;
 
 	/**
-	 * @var string ID e tregtarit. Jepet nga banka.
+	 * @var string ID e terminalit. Jepet nga banka
 	 */
-	private $merchant_id;
+	private $terminalId;
 
 	/**
-	 * @var string ID e terminalit. Jepet nga banka.
+	 * @var mixed Totali i parave qe duhen paguar
 	 */
-	private $terminal_id;
+	private $totalAmount;
 
 	/**
-	 * @var mixed Totali i parave qe duhen paguar.
+	 * @var string Koha e blerjes
 	 */
-	private $total_amount;
+	private $purchaseTime;
 
 	/**
-	 * @var string Koha e blerjes.
+	 * @var string ID e porosise
 	 */
-	private $purchase_time;
+	private $orderId;
 
 	/**
-	 * @var string ID e porosise.
+	 * @var string ID e valutes. Paravendosur ne "LEK"
 	 */
-	private $order_id;
+	private $currencyId = '008';
 
 	/**
-	 * @var string ID e valutes. Paravendosur ne "LEK".
+	 * @var string ID e valutes. Paravendosur ne "LEK"
 	 */
-	private $currency_id = '008';
+	private $sessionData = '';
 
 	/**
-	 * @var string ID e valutes. Paravendosur ne "LEK".
+	 * @var string Direktoria ku ndodhet certifikata
 	 */
-	private $session_data = '';
+	private $certDir = 'cert';
 
 	/**
-	 * @var string Direktoria ku ndodhet certifikata.
+	 * @var string Kodet e valutes
 	 */
-	private $cert_dir = 'cert';
-
-	/**
-	 * @var string Kodet e valutes.
-	 */
-	private $currency_codes = array(
+	private $currencyCodes = [
 		'all' => '008',
 		'usd' => '840',
 		'eur' => '978'
-	);
+	];
 
 	/**
 	 * Constructor. Vendos te dhenat qe kerkohen per te gjeneruar
-	 * nje signature.
+	 * nje signature
 	 * 
-	 * @param string $merchant_id
-	 * @param string $terminal_id
-	 * @param mixed $total_amount Totali i parave qe duhen paguar
+	 * @param string $merchantId
+	 * @param string $terminalId
+	 * @param mixed $totalAmount Totali i parave qe duhen paguar
 	 * @param array $options Opsione ekstra: 'purchase_time', 'order_id', 'currency_id', 'cert_dir'
 	 * @return void
 	 */
-	public function __construct($merchant_id, $terminal_id, $total_amount, $options = array())
+	public function __construct($merchantId, $terminalId, $totalAmount, array $options = [])
 	{
-		$this->merchant_id = $merchant_id;
-		$this->terminal_id = $terminal_id;
-		$this->total_amount = $total_amount;
+		$this->merchantId = $merchantId;
+		$this->terminalId = $terminalId;
+		$this->totalAmount = $totalAmount;
 
 		$this->readOptions($options);
 		$this->defaultOptions();
 	}
 
 	/**
-	 * Factory per ta gjeneruar klasen pa nevojen
-	 * e therritjes se operatorit "new".
-	 * 
-	 * @param string $merchant_id
-	 * @param string $terminal_id
-	 * @param mixed $total_amount
-	 * @param array $options
-	 * @return Authenticate
-	 */
-	public static function factory($merchant_id, $terminal_id, $total_amount, $options = array())
-	{
-		return new self($merchant_id, $terminal_id, $total_amount, $options = array());
-	}
-
-	/**
-	 * Vendos opsionet e percaktuara nga klienti.
+	 * Vendos opsionet e percaktuara nga klienti
 	 * 
 	 * @return void
 	 */
 	private function readOptions($options)
 	{
 		// Nese s'ka opsione, mos vazhdo.
-		if (! count($options)) return;
+		if (!count($options)) return;
 
-		if (isset($options['purchase_time'])) $this->purchase_time = $options['purchase_time'];
+		if (isset($options['purchase_time'])) {
+			$this->purchaseTime = $options['purchase_time'];
+		}
 
-		if (isset($options['order_id']))
-		{
+		if (isset($options['order_id'])) {
 			// ID e porosise mund te kalohet edhe si funksion
-			// anonim. is_callable() kontrollon nese eshte i tille.
-			if (is_callable($options['order_id']))
-			{
-				// Vendos vleren e order_id me ate qe kthen funksioni.
-				$this->order_id = call_user_func($options['order_id']);
+			// anonim. is_callable() kontrollon nese eshte i tille
+			if (is_callable($options['order_id'])) {
+				// Vendos vleren e orderId me ate qe kthen funksioni
+				$this->orderId = call_user_func($options['order_id']);
 			}
-			else
-			{
-				$this->order_id = $options['order_id'];
+			else {
+				$this->orderId = $options['order_id'];
 			}
 		}
 		
 		// Valuta duhet te jete vendosur dhe te egzistoje tek
-		// lista e valutave te pranuara.
-		if (isset($options['currency_id']) and isset($this->currency_codes[$options['currency_id']]))
-		{
-			$this->currency_id = $this->currency_codes[$options['currency_id']];
+		// lista e valutave te pranuara
+		if (isset($options['currency_id']) and isset($this->currencyCodes[$options['currency_id']])) {
+			$this->currencyId = $this->currencyCodes[$options['currency_id']];
 		}
 
-		if (isset($options['session_data'])) $this->session_data = $options['session_data'];
-		if (isset($options['cert_dir'])) $this->cert_dir = rtrim($options['cert_dir'], '/\\');
+		if (isset($options['session_data'])) $this->sessionData = $options['session_data'];
+		if (isset($options['cert_dir'])) $this->certDir = rtrim($options['cert_dir'], '/\\');
 	}
 
 	/**
 	 * Vendos disa opsione baze nese nuk jane percaktuar
-	 * me pare nga klienti.
+	 * me pare nga klienti
 	 * 
 	 * @return void
 	 */
 	private function defaultOptions()
 	{
-		// 'ymdHis' eshte formati i kohes qe banka
-		// e kerkon.
-		if (! $this->purchase_time) $this->purchase_time = date('ymdHis');
+		// 'ymdHis' eshte formati i kohes qe kerkon banka
+		if (! $this->purchaseTime) {
+			$this->purchaseTime = date('ymdHis');
+		}
 
-		// Gjenero nje ID porosie.
-		if (! $this->order_id) $this->order_id = uniqid();
+		// Gjenero nje ID porosie
+		if (! $this->orderId) {
+			$this->orderId = uniqid();
+		}
 	}
 
 	/**
 	 * Kthen te dhenat si array per tu perdorur me pas
-	 * ne formen e kerkeses.
+	 * ne formen e kerkeses
 	 * 
 	 * @return array
 	 */
@@ -164,30 +146,30 @@ class Authenticate
 	{
 		$signature = $this->generateSignature();
 
-		return array(
-			'merchant_id' => $this->merchant_id,
-			'terminal_id' => $this->terminal_id,
-			'total_amount' => $this->total_amount,
-			'currency_id' => $this->currency_id,
-			'purchase_time' => $this->purchase_time,
-			'order_id' => $this->order_id,
-			'session_data' => $this->session_data,
+		return [
+			'merchant_id' => $this->merchantId,
+			'terminal_id' => $this->terminalId,
+			'total_amount' => $this->totalAmount,
+			'currency_id' => $this->currencyId,
+			'purchase_time' => $this->purchaseTime,
+			'order_id' => $this->orderId,
+			'session_data' => $this->sessionData,
 			'signature' => $signature
-		);
+		];
 	}
 
 	/**
-	 * Gjeneron signature.
+	 * Gjeneron signature
 	 * 
 	 * @return string
 	 */
 	private function generateSignature()
 	{
-		// Formati i te dhenave ne baze te manualit.
+		// Formati i te dhenave ne baze te manualit
 		$data = $this->formatData();
 
-		// Lexo certifikaten dhe shenjoje me OpenSSL.
-		$key = file_get_contents("$this->cert_dir/$this->merchant_id.pem");
+		// Lexo certifikaten dhe shenjoje me OpenSSL
+		$key = file_get_contents("$this->certDir/$this->merchantId.pem");
 		$key = openssl_get_privatekey($key);
 		openssl_sign($data, $signature, $key);
 		openssl_free_key($key);
@@ -197,13 +179,13 @@ class Authenticate
 
 	/**
 	 * Formaton te dhenat per tu perdorur ne
-	 * gjenerimin e signature.
+	 * gjenerimin e signature
 	 * 
 	 * @return string
 	 */
 	public function formatData()
 	{
-		return "$this->merchant_id;$this->terminal_id;$this->purchase_time;$this->order_id;$this->currency_id;$this->total_amount;$this->session_data;";
+		return "$this->merchantId;$this->terminalId;$this->purchaseTime;$this->orderId;$this->currencyId;$this->totalAmount;$this->sessionData;";
 	}
 
 }
--- src/Notify.php
+++ src/Notify.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Fadion\Raiffeisen;
 
 /**
@@ -10,67 +11,48 @@ namespace Fadion\Raiffeisen;
  * NOTIFY_URL e Gateway, qe ben konfirmimin duke
  * komunikuar server me server dhe jo me opsionet
  * me pak te sigurta SUCCESS_URL/FAILURE_URL.
- *
- * @package Raiffeisen\Notify
- * @author Fadion Dashi
- * @version 1.1.0
  */
 class Notify
 {
-
 	/**
-	 * @var string URL kur blerja konfirmohet.
+	 * @var string URL kur blerja konfirmohet
 	 */
-	private $success_url;
+	private $successUrl;
 
 	/**
-	 * @var string URL kur blerja nuk konfirmohet.
+	 * @var string URL kur blerja nuk konfirmohet
 	 */
-	private $error_url;
+	private $errorUrl;
 
 	/**
-	 * @var string Do te mbaje URL-ne e suksesit apo deshtimit.
+	 * @var string Do te mbaje URL-ne e suksesit apo deshtimit
 	 */
 	private $url;
 
 	/**
-	 * @var array Do te mbaje superglobalen $_POST.
+	 * @var array Do te mbaje superglobalen $_POST
 	 */
 	private $post;
 
 	/**
 	 * Constructor. Vendos URL-ne e suksesit dhe deshtimit, dhe
-	 * vlerat nga superglobalja $_POST.
+	 * vlerat nga superglobalja $_POST
 	 * 
-	 * @param string $success_url
-	 * @param string $error_url
+	 * @param string $successUrl
+	 * @param string $errorUrl
 	 * @param array $total Superglobalja $_POST
 	 * @return void
 	 */
-	public function __construct($success_url, $error_url, $post_data)
+	public function __construct($successUrl, $errorUrl, $postData)
 	{
-		$this->success_url = $success_url;
-		$this->error_url = $error_url;
-		$this->post = $post_data;
-	}
-
-	/**
-	 * Factory per ta gjeneruar klasen pa nevojen
-	 * e therritjes se operatorit "new".
-	 * 
-	 * @param string $success_url
-	 * @param string $error_url
-	 * @param array $total
-	 * @return Notify
-	 */
-	public static function factory($success_url, $error_url, $post_data)
-	{
-		return new self($success_url, $error_url, $post_data);
+		$this->successUrl = $successUrl;
+		$this->errorUrl = $errorUrl;
+		$this->post = $postData;
 	}
 
 	/**
 	 * Kontrollon nese kerkesa nga serveri i Gateway
-	 * eshte e vlefshme.
+	 * eshte e vlefshme
 	 * 
 	 * @param string $ip IP e serverit te Gateway nga pritet kerkesa
 	 * @return bool
@@ -80,10 +62,10 @@ class Notify
 		// Kodi i Transaksionit (TranCode) duhet te jete '000', qe
 		// tregon se transaksioni ne anen e bankes ka qene i suksesshem.
 		// Kontrollohet edhe referuesi dhe IP gjithashtu, per te tentuar
-		// te shmange kerkesat e nisura manualisht nga dashakeqes.
+		// te shmange kerkesat e nisura manualisht nga dashakeqes
 		if ($this->post['TranCode'] == '000' and 
-			($_SERVER['HTTP_REFERER'] == $ip or $_SERVER['REMOTE_ADDR'] == $ip))
-		{
+			($_SERVER['HTTP_REFERER'] == $ip or $_SERVER['REMOTE_ADDR'] == $ip)
+		) {
 			return true;
 		}
 
@@ -95,13 +77,13 @@ class Notify
 	 * dhe duhet te procedohet me pagesen. Metoda duhet
 	 * te therritet pasi aplikacioni te kete kryer me
 	 * sukses veprimet ne databaze apo cfaredo strategjie
-	 * perdoret per te ruajtur blerjet.
+	 * perdoret per te ruajtur blerjet
 	 * 
 	 * @return string
 	 */
 	public function success()
 	{
-		$this->url = $this->success_url;
+		$this->url = $this->successUrl;
 
 		return $this->output('success');
 	}
@@ -118,7 +100,7 @@ class Notify
 	 */
 	public function error($reason = '')
 	{
-		$this->url = $this->error_url;
+		$this->url = $this->errorUrl;
 
 		return $this->output('reverse', $reason);
 	}
